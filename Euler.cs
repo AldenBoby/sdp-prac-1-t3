4ca3b80152a0c0524580686c10c4983caddb5fce
William Stapleton <G17S9109@ict.ru.ac.za>
2020-02-13 15:53:00 +0200

    Start of the thing

---
+++ Euler.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+public class Euler(){
+    isPrime(10);
+}
+   
+ private bool isPrime(int num){
+               if(counter < 2){
+            return true;
+        }else{
+            return false;
+        }
+ }
+
+ private void printPrimes(){
+     
+ }
