5541e6e6b2f24460449b259f1f920aa59b97c4eb
AldenBoby <boby.alden128@gmail.com>
2020-02-15 19:50:36 +0200

    Added solution for Euler problem 1 _Alden

---
+++ Euler/Problem 1/Program.cs
@@ -0,0 +1,27 @@
+﻿///Euler Problem 1 solution by Alden Boby 
+///2020
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ans = 0;
+            for (int n = 0; n < 1000; n++)
+            {
+                if (n % 3 == 0 || n % 5 == 0) //finds multiples of five or three
+                {
+                    ans += n; // adds only the multiples
+                }
+            }
+            Console.WriteLine(ans);
+            Console.ReadKey();
+        }
+    }
+}
