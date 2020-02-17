0812a618139d5184b9b8a9e21df26a206f0665a4
Nathan <a1@1s-MacBook-Pro.local>
2020-02-17 21:46:04 +0200

    Nate comes again

---
+++ Euler/Problem 2/Program.cs
@@ -0,0 +1,55 @@
+﻿///Euler Problem 1 solution by Alden Boby 
+///2020
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+ namespace Euler
+  {
+   internal class Program
+   {
+    private static int Palindroming(int a)
+    {
+        var results = 0;
+
+
+        while (a > 0)
+        {
+            results = results * 10 + a % 10;
+            a /= 10;
+        }
+
+
+        return results;
+    }
+
+    private static void Main(string[] args)
+    {
+        var multiply = 0;
+        var largestMultiply = 0;
+        var largest = 0;
+        for (var i = 100; i <= 999; i++)
+        for (var j = 100; j <= 999; j++)
+        {
+            var temp = multiply;
+            multiply = i * j;
+
+            if (multiply == Palindroming(multiply))
+                if (temp > largestMultiply)
+                {
+                    largestMultiply = temp;
+
+                    if (multiply > largestMultiply)
+                        largest = multiply;
+                }
+        }
+
+        Console.WriteLine(largest);
+        Console.ReadLine();
+    }
+  }
+}
